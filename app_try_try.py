a9c394dfd4078077ad015abf7deef0b4cb192c09
Purushothaman <136145176+Krathor2212@users.noreply.github.com>
2024-09-01 13:49:22 +0530

    Update app_try_try.py

--- app_try_try.py
+++ app_try_try.py
@@ -15,14 +15,12 @@ model_status = {
 }
 
 # Path where the model will be saved
-model_dir = os.path.join(os.getcwd(), 'model')
-model_path = os.path.join(model_dir, 'classifier.pkl')
+model_path = os.path.join(os.getcwd(), 'model', 'classifier.pkl')
 
 def download_model():
     global model_status
-    model_status['status'] = 'Downloading'
     try:
-        os.makedirs(model_dir, exist_ok=True)  # Ensure model directory exists
+        model_status['status'] = 'Downloading'
         model_url = 'https://drive.google.com/uc?export=download&id=1Nshs0xEK-5XAzL8shnKzpd5mldz2WfF9'
         gdown.download(model_url, model_path, quiet=False)
         model_status['status'] = 'Downloaded'
@@ -31,29 +29,33 @@ def download_model():
         model_status['status'] = 'Error'
         model_status['message'] = f'Error downloading model: {str(e)}'
 
-def load_model():
-    global model_status
-    try:
-        global classifier
-        classifier = joblib.load(model_path)
-        model_status['status'] = 'Ready'
-        model_status['message'] = 'Model loaded successfully.'
-    except Exception as e:
-        model_status['status'] = 'Error'
-        model_status['message'] = f'Error loading model: {str(e)}'
-
 # Check if the model already exists locally
 if not os.path.exists(model_path):
     download_model()
-load_model()
+else:
+    model_status['status'] = 'Loaded'
+    model_status['message'] = 'Model already exists locally.'
 
-def predict_func(reviews):
+try:
+    # Load the model
+    classifier = joblib.load(model_path)
+    model_status['status'] = 'Ready'
+    model_status['message'] = 'Model loaded successfully.'
+except Exception as e:
+    model_status['status'] = 'Error'
+    model_status['message'] = f'Error loading model: {str(e)}'
+
+def predictfunc(reviews):
     predictions = classifier.predict(reviews)
-    return int(predictions[0])
+    predict = int(predictions[0])
+    return predict
 
 @app.route('/')
 def index():
+    # Redirect to the model status page or return a simple welcome message
     return redirect(url_for('get_model_status'))
+    # Alternatively, return a simple HTML message
+    # return "<h1>Welcome to the Sentiment Analysis API</h1>"
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
@@ -65,8 +67,9 @@ def analyze():
 
     if message:
         review = pd.Series(message)
-        prediction = predict_func(review)
+        prediction = predictfunc(review)
         is_bully = prediction == 0
+        print("bully", is_bully)
         response = {
             'prediction': prediction,
             'is_bullying': is_bully
