5920708370c47a32a607d693780d72158d961319
Purushothaman <136145176+Krathor2212@users.noreply.github.com>
2024-09-01 14:00:19 +0530

    Update app_try_try.py

--- app_try_try.py
+++ app_try_try.py
@@ -14,8 +14,9 @@ model_status = {
     'message': ''
 }
 
-# Path where the model will be saved
-model_path = os.path.join(os.getcwd(), 'model', 'classifier.pkl')
+# Directory and path where the model will be saved
+model_dir = os.path.join(os.getcwd(), 'model')
+model_path = os.path.join(model_dir, 'classifier.pkl')
 
 def download_model():
     global model_status
@@ -32,7 +33,6 @@ def download_model():
     except Exception as e:
         model_status['status'] = 'Error'
         model_status['message'] = f'Error downloading model: {str(e)}'
-
 
 # Check if the model already exists locally
 if not os.path.exists(model_path):
@@ -59,8 +59,6 @@ def predictfunc(reviews):
 def index():
     # Redirect to the model status page or return a simple welcome message
     return redirect(url_for('get_model_status'))
-    # Alternatively, return a simple HTML message
-    # return "<h1>Welcome to the Sentiment Analysis API</h1>"
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
@@ -71,7 +69,7 @@ def analyze():
     message = data.get('message', '')
 
     if message:
-        review = pd.Series(message)
+        review = pd.Series([message])  # Wrap the message in a list
         prediction = predictfunc(review)
         is_bully = prediction == 0
         print("bully", is_bully)
